0ab4e7aa49763bbeb67bfe7f68c864aa9d6c7e84
Denys Kryvtsov <denyskryvtsov@gmail.com>
2023-02-03 17:49:16 +0200

    add main.cpp

---
+++ Lab1/Task3.1/Task3.1/Task3.1.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+#include <string>
+
+#include "CarSimulator.h"
+
+int main()
+{   
+    CarSimulator cS("BMW", 15.2, 6.0, 100);
+    
+    cS.m_carBuilder.show(cS.getCar());
+    return 0;
+}
+
